935e0fcfb133bb9bb759111734a03dfb14d62ba6
rafaelmachadomolina <70354640+rafaelmachadomolina@users.noreply.github.com>
2022-04-15 13:20:48 +0100

    Updated Airbnb batch README and schema initialisation.

--- BatchData_airbnb/README.md
+++ BatchData_airbnb/README.md
@@ -1,3 +1,54 @@
-#ETL for batch processing Airbnb data to the application
+<!-- ABOUT THE PROJECT -->
+# ETL for batch processing Airbnb data to the application
 
-This file must include a diagram of the ETL stage, and data traceability (including OS et al).
+This section contains the main ETL to process the obtained data from Airbnb. There are two stages:
+- Exploratory data analysis using the Jupyter notebook `EDA-airbinb.ipynb`
+- ETL to move the data from *csv* files into the database using pySpark `ETL-airbinb.ipynb`
+
+The latter section presents the proposed schema for the solution, inferred from the data but also enriched to better exploit features. In addition, the notebook saves a backup of the schema in a dedicated S3 bucket, backup format is *parquet*.
+
+### Built With
+
+* [Python3](https://www.python.org/downloads/)
+* [pySpark](https://spark.apache.org/docs/latest/api/python/)
+* [Great Expectations](https://greatexpectations.io)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+A few setups are required to fully run the ETL.
+
+### Prerequisites
+
+An environment with Python3 will suffice up to this point. No additional software is previously required.
+
+### Installation
+
+1. Run the installation script to get Java, Spark and the JDBC driver for PostgreSQL.
+  ```sh
+  bash init_spark.sh
+  ```
+2. Install *Great Expectations* on a Python virtual env. This framework is later used to validate data quality.
+   ```sh
+   pip install great_expectations
+   ```
+   
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## Contact
+
+Rafael Machado Molina - [@rafaelmachadom](https://twitter.com/rafaelmachadom) - r.machado.molina@gmail.com
+
+Project Link: [https://github.com/rafaelmachadomolina/fareApp](https://github.com/rafaelmachadomolina/fareApp)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* [Great Expectations](https://github.com/great-expectations/great_expectations)
+* [Best README template by othneildrew](https://github.com/othneildrew/Best-README-Template)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
---
+++ PostgreSQL/Queries/Create listings schema.sql
@@ -0,0 +1,142 @@
+--This file creates the main schema for listings, as well as the tables
+
+--Create schema
+CREATE SCHEMA listings;
+
+--Drop tables if exist
+DROP TABLE IF EXISTS listings.calendar;
+DROP TABLE IF EXISTS listings.host_verification;
+DROP TABLE IF EXISTS listings.hosts;
+DROP TABLE IF EXISTS listings.listing_amenities;
+DROP TABLE IF EXISTS listings.listing_complements;
+DROP TABLE IF EXISTS listings.listings;
+DROP TABLE IF EXISTS listings.neighbourhoods;
+DROP TABLE IF EXISTS listings.reviews;
+
+--Create tables with schema
+CREATE TABLE listings.calendar (
+  id BIGINT NOT NULL PRIMARY KEY,
+  listing_id BIGINT NOT NULL,
+  date DATE,
+  available BOOLEAN,
+  price NUMERIC(9, 2),
+  adjusted_price NUMERIC(9, 2),
+  minimum_nights INT,
+  maximum_nights INT
+);
+
+CREATE TABLE listings.host_verification (
+  id BIGINT NOT NULL PRIMARY KEY,
+  host_id BIGINT NOT NULL,
+  verification TEXT
+);
+
+CREATE TABLE listings.hosts (
+  id BIGINT NOT NULL PRIMARY KEY,
+  url TEXT,
+  name TEXT,
+  since DATE,
+  location TEXT,
+  about TEXT,
+  response_time TEXT,
+  response_rate NUMERIC(9, 2),
+  acceptance_rate NUMERIC(9, 2),
+  is_superhost BOOLEAN,
+  thumbnail_url TEXT,
+  picture_url TEXT,
+  neighbourhood TEXT,
+  listings_count INT,
+  total_listings_count INT,
+  has_profile_pic BOOLEAN,
+  identity_verified BOOLEAN
+);
+
+CREATE TABLE listings.listing_amenities (
+  id BIGINT NOT NULL PRIMARY KEY,
+  listing_id BIGINT NOT NULL,
+  amenity VARCHAR(200)
+);
+
+CREATE TABLE listings.listing_complements (
+  id BIGINT NOT NULL PRIMARY KEY,
+  listing_id BIGINT NOT NULL,
+  minimum_minimum_nights INT,
+  maximum_minimum_nights INT,
+  minimum_maximum_nights INT,
+  maximum_maximum_nights INT,
+  minimum_nights_avg_ntm DECIMAL(19, 2),
+  maximum_nights_avg_ntm DECIMAL(19, 2),
+  has_availability BOOLEAN,
+  availability_30 INT,
+  availability_60 INT,
+  availability_90 INT,
+  availability_365 INT,
+  calendar_last_scraped DATE,
+  number_of_reviews INT,
+  number_of_reviews_ltm INT,
+  number_of_reviews_l30d INT,
+  first_review DATE,
+  last_review DATE,
+  review_scores_rating DECIMAL(9, 2),
+  review_scores_accuracy DECIMAL(9, 2),
+  review_scores_cleanliness DECIMAL(9, 2),
+  review_scores_checkin DECIMAL(9, 2),
+  review_scores_communication DECIMAL(9, 2),
+  review_scores_location DECIMAL(9, 2),
+  review_scores_value DECIMAL(9, 2),
+  instant_bookable BOOLEAN,
+  calculated_host_listings_count INT,
+  calculated_host_listings_count_entire_homes INT,
+  calculated_host_listings_count_private_rooms INT,
+  calculated_host_listings_count_shared_rooms INT,
+  reviews_per_month DECIMAL(9, 2)
+);
+
+CREATE TABLE listings.listings (
+  id BIGINT NOT NULL PRIMARY KEY,
+  host_id BIGINT NOT NULL,
+  listing_url TEXT,
+  scrape_id BIGINT,
+  date_last_scraped DATE,
+  name TEXT,
+  description TEXT,
+  neighborhood_overview TEXT,
+  picture_url TEXT,
+  neighbourhood_typed TEXT,
+  neighbourhood_group VARCHAR(200),
+  latitude DECIMAL(9, 2),
+  longitude DECIMAL(9, 2),
+  property_type TEXT,
+  room_type TEXT,
+  accommodates INT,
+  bathrooms_text TEXT,
+  bedrooms INT,
+  beds INT,
+  price DECIMAL(9, 2),
+  minimum_nights INT,
+  maximum_nights INT
+);
+
+CREATE TABLE listings.neighbourhoods (
+  id BIGINT NOT NULL PRIMARY KEY,
+  neighbourhood VARCHAR(200)
+);
+
+CREATE TABLE listings.reviews (
+  id BIGINT NOT NULL PRIMARY KEY,
+  listing_id BIGINT NOT NULL,
+  date DATE,
+  reviewer_id BIGINT,
+  reviewer_name VARCHAR(200),
+  comments TEXT
+);
+
+--Grant all privileges for manipulating data on the schema to the Faculty service user
+GRANT ALL ON listings.calendar TO service_user_faculty;
+GRANT ALL ON listings.host_verification TO service_user_faculty;
+GRANT ALL ON listings.hosts TO service_user_faculty;
+GRANT ALL ON listings.listing_amenities TO service_user_faculty;
+GRANT ALL ON listings.listing_complements TO service_user_faculty;
+GRANT ALL ON listings.listings TO service_user_faculty;
+GRANT ALL ON listings.neighbourhoods TO service_user_faculty;
+GRANT ALL ON listings.reviews TO service_user_faculty;
